d45220fae066bae38ce796300c39f687689757c6
EasyGameBoy <easygameboyiphone@gmail.com>
2025-03-04 09:03:11 +0100

    reizen

--- src/main/java/org/example/logic/Reizen.java
+++ src/main/java/org/example/logic/Reizen.java
@@ -1,27 +1,36 @@
-/*
 package org.example.logic;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Reizen {
     private int aktuellerReizwert;
     private int hoechsterReizwert;
     private int gewinner;
+    private List<Integer> reizTabelle = new ArrayList<>();
 
-    public int getHoechsterReizwert() {
-        return hoechsterReizwert;
-    }
-
-    public void setHoechsterReizwert(int hoechsterReizwert) {
-        this.hoechsterReizwert = hoechsterReizwert;
-    }
 
 
     public Reizen() {   //Konstruktor
         this.aktuellerReizwert = 18;
         this.hoechsterReizwert = 0;
         this.gewinner = -1;
-        sv.aktuellerSpieler = 0;
+        reizTabelle.addAll(Arrays.asList(18, 20, 22, 23, 24, 27, 30, 33, 35, 36, 40, 44, 45, 46, 48, 50, 55, 59, 60, 72, 96, 120));
+        //sv.aktuellerSpieler = 0;
     }
+
+
+    public int getHoechsterReizwert() {
+        return hoechsterReizwert;
+    }
+
+
+    public void setHoechsterReizwert(int hoechsterReizwert) {
+        this.hoechsterReizwert = hoechsterReizwert;
+    }
+
 
     public boolean reizen (int spielerIndex, int caseIndex){
         int reizwert = getReizwert(caseIndex);
@@ -29,67 +38,19 @@ public class Reizen {
             aktuellerReizwert = reizwert;
             hoechsterReizwert = reizwert;
             gewinner = spielerIndex;
-            sv.naechsterSpieler();
+            //sv.naechsterSpieler();
             return true;
         }
         return false;
     }
 
     public void passe (int spielerIndex){
-        sv.naechsterSpieler();
+        //sv.naechsterSpieler();
         if (spielerIndex == gewinner) {
         }
     }
 
     private int getReizwert(int caseIndex){
-        switch (caseIndex) {
-            case 0:
-                return 18;
-            case 1:
-                return 20;
-            case 2:
-                return 22;
-            case 3:
-                return 23;
-            case 4:
-                return 24;
-            case 5:
-                return 27;
-            case 6:
-                return 30;
-            case 7:
-                return 33;
-            case 8:
-                return 35;
-            case 9:
-                return 36;
-            case 10:
-                return 40;
-            case 11:
-                return 44;
-            case 12:
-                return 45;
-            case 13:
-                return 46;
-            case 14:
-                return 48;
-            case 15:
-                return 50;
-            case 16:
-                return 55;
-            case 17:
-                return 59;
-            case 18:
-                return 60;
-            case 19:
-                return 72;
-            case 20:
-                return 96;
-            case 21:
-                return 120;
-            default:
-                return 0;
-        }
+        return reizTabelle.get(caseIndex);
     }
 }
- */
